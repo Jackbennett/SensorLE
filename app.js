b6e69dd4cf8b2483fd60b580d47cabd38fb30385
JackBennett <me@jackben.net>
2016-03-24 23:33:53 +0000

    Graph drawn with d3 depends on data value and scales

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 var five = require('johnny-five')
+
 var board = new five.Board({port: 'COM3'})
 
+app.use(express.static(__dirname))
+
 board.on("ready", function() {
+	var last = 0
 	this.pinMode(0, five.Pin.ANALOG)
 	this.analogRead(0, voltage =>{
-		console.log(voltage)
+		if(last !== voltage){
+			io.emit("data", {"value": voltage})
+			last = voltage
+		}
 	})
+})
+
+http.listen(3000, '0.0.0.0', () =>{
+	console.log('listening')
 })
--- js/chart.js
+++ js/chart.js
@@ -1,13 +1,25 @@
+var socket = io()
+var sensor = {"pressure": 500}
+
+document.addEventListener('DOMContentLoaded', function(){
+
 var w = 960,
     h = 500,
     tau = Math.PI * 2,
     good = '#E6AC27',
     bad = '#BF4D28'
 
+var scale = d3.scale.linear()
+  .domain([0, 1023])
+  .range([0, Math.PI])
+
 var arc = d3.svg.arc()
   .innerRadius(180)
   .outerRadius(240)
   .startAngle(0)
+  .endAngle(function(d){
+    return scale(d.pressure)
+  })
 
 var svg = d3.select('#chart')
   .append('svg')
@@ -16,7 +28,10 @@ var svg = d3.select('#chart')
   .append('g')
     .attr('transform', 'translate(' + w /2 + ', ' + h / 2 + '), rotate(-90)')
 
-var bar = svg.append('path')
-  .datum({endAngle: .3 * Math.PI })
-  .style('fill', bad)
-  .attr("d", arc)
+var bar = svg.selectAll('path')
+  .data([sensor])
+  .enter()
+    .append('path')
+    .attr("d", arc)
+    .style('fill', bad)
+})
