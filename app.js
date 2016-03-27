b50ae85fd0df7d681669a600c8f17af5d2784149
JackBennett <me@jackben.net>
2016-03-27 14:39:54 +0100

    starting on making the d3 chart more contained

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var five = require('johnny-five')
 
-var board = new five.Board({port: 'COM3'})
+var board //= new five.Board({port: 'COM3'})
 
 app.use(express.static(__dirname))
 
-board.on("ready", function() {
-	var last = 0
-	this.pinMode(0, five.Pin.ANALOG)
-	this.analogRead(0, voltage =>{
-		if(last !== voltage){
-			io.emit("data", {"pressure": voltage})
-			last = voltage
-		}
-	})
-})
+//board.on("ready", function() {
+//	var last = 0
+//	this.pinMode(0, five.Pin.ANALOG)
+//	this.analogRead(0, voltage =>{
+//		if(last !== voltage){
+//			io.emit("data", {"pressure": voltage})
+//			last = voltage
+//		}
+//	})
+//})
 
 http.listen(3000, '0.0.0.0', () =>{
 	console.log('listening')
--- js/chart.js
+++ js/chart.js
@@ -1,28 +1,55 @@
-var socket = io()
+window.socket = io()
+window.gauge = function(options){
+  'use strict'
+  
+  options = options || {}
+  
+  var d3 = window.d3
+  var w, h, 
+  var innerRadius = options.innerRadius || 180
+  var outerRadius = options.outerRadius || 240 
+  var data = options.data || []
+  
+  var arc = d3.svg.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
+    .startAngle(0)
+    .endAngle(function(d){
+      return scale(d.pressure)
+    })
+  
+  var scale = d3.scale.linear()
+    .domain([0, 1023])
+    .range([0.01, Math.PI])
+  
+  var fill = d3.scale.linear()
+    .domain([0,1023])
+    .range(['#BF4D28', '#E6AC27'])
+  
+  var svg = d3.select("#chart").append("svg")
+  
+  function chart(){
+    var bar = svg.selectAll('path')
+      .data([data])
+
+    bar.enter()
+      .append('path')
+      .attr("d", arc)
+
+    bar.attr("d", arc)
+      .style("fill", function(d){return color(d.pressure)})
+
+    bar.exit().remove()
+  }
+}
+
 
 document.addEventListener('DOMContentLoaded', function(){
 
 var w = 960,
     h = 500,
-    tau = Math.PI * 2,
-    good = '#E6AC27',
-    bad = '#BF4D28'
 
-var scale = d3.scale.linear()
-  .domain([0, 1023])
-  .range([0.01, Math.PI])
 
-var color = d3.scale.linear()
-  .domain([0,1023])
-  .range([bad, good])
-
-var arc = d3.svg.arc()
-  .innerRadius(180)
-  .outerRadius(240)
-  .startAngle(0)
-  .endAngle(function(d){
-    return scale(d.pressure)
-  })
 
 var svg = d3.select('#chart')
   .append('svg')
@@ -30,21 +57,6 @@ var svg = d3.select('#chart')
     .attr('height', h)
   .append('g')
     .attr('transform', 'translate(' + w /2 + ', ' + h / 2 + '), rotate(-90)')
-
-function update(data){
-  var bar = svg.selectAll('path')
-    .data([data])
-
-  bar.enter()
-      .append('path')
-      .attr("d", arc)
-      .style("fill", bad)
-
-  bar.attr("d", arc)
-    .style("fill", function(d){return color(d.pressure)})
-
-  bar.exit().remove()
-}
 
 update({"pressure": 500})
 
