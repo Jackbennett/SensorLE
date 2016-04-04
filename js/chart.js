f2116e94d2788b851cab0fa0541d009d7c7d4d19
JackBennett <me@jackben.net>
2016-04-04 12:08:08 +0100

    minor whitespace fix

--- js/chart.js
+++ js/chart.js
@@ -1,14 +1,14 @@
 window.gauge = function(options){
   'use strict'
-  
+
   options = options || {}
-  
+
   var d3 = window.d3
   var w = 500
   var h = 250
   var innerRadius = options.innerRadius || 180
-  var outerRadius = options.outerRadius || 240 
-  
+  var outerRadius = options.outerRadius || 240
+
   var arc = d3.svg.arc()
     .innerRadius(innerRadius)
     .outerRadius(outerRadius)
@@ -16,22 +16,22 @@ window.gauge = function(options){
     .endAngle(function(d){
       return scale(d.pressure)
     })
-  
+
   var scale = d3.scale.linear()
     .domain([0, 255])
     .range([0.01, Math.PI])
-  
+
   var fill = d3.scale.linear()
     .domain([0,255])
     .range(['#BF4D28', '#E6AC27'])
-  
+
   var svg = d3.select("#chart").append("svg")
     .attr('width', w)
     .attr('height', h)
     .append('g')
       .attr('transform', 'translate(' + w /2 + ', ' + h + '), rotate(-90)')
 
-  
+
   function update(data){
     var data = data || options.data || {pressure: 250} // Mock Data
     var bar = svg.selectAll('path')
