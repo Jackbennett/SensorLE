048f75efc6b8c010986331c231ccdf4a850b6d2a
JackBennett <me@jackben.net>
2016-04-04 12:06:44 +0100

    Add board fail message. changing indent style

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var five = require('johnny-five')
 
-var board //= new five.Board({port: 'COM3'})
+var board = new five.Board()
 
 app.use(express.static(__dirname))
 
-//board.on("ready", function() {
-//	var last = 0
-//	this.pinMode(0, five.Pin.ANALOG)
-//	this.analogRead(0, voltage =>{
-//		if(last !== voltage){
-//			io.emit("data", {"pressure": voltage})
-//			last = voltage
-//		}
-//	})
-//})
+board.on("ready", function() {
+  var last = 0
+  this.pinMode(0, five.Pin.ANALOG)
+  this.analogRead(0, voltage =>{
+  if(last !== voltage){
+    io.emit("data", {"pressure": voltage})
+    last = voltage
+    }
+  })
+})
+board.on("fail", function() {
+  console.log('board not found. Not attached?')
+})
 
 http.listen(3000, '0.0.0.0', () =>{
-	console.log('listening')
+  console.log('listening')
 })
